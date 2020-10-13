5aa5460cdc1121c95f67a21588c9406bb8db01a1
vanessa928 <72827450+vanessa928@users.noreply.github.com>
2020-10-13 17:47:44 -0500

    Add files via upload

---
+++ database/migrations/2020_09_23_045832_create_entrances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntrancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entrances', function (Blueprint $table) {
+            $table->id();
+            $table->float('price', 9, 2);
+            $table->time('hourEntrance', 0);
+            $table->string('salas_id', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entrances');
+    }
+}
